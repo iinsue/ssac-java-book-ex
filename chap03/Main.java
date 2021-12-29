561149577e5e37950a24690a91de76cc9a2cc9fa
iinsue <hinsu93@naver.com>
2021-12-29 10:27:54 +0900

    chapter4.2.2 if-else if-else

--- chap03/Main.java
+++ chap03/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        //Exercise04.exer04();
-        //Exercise05.exer05();
+        Exercise04.exer04();
+        Exercise05.exer05();
         Exercise06.exer06();
     }
 }
---
+++ chap04/Execute.java
@@ -0,0 +1,5 @@
+public class Execute {
+    public static void main(String[] args) {
+        IfElseIfElseExample.ifelseifelse();
+    }
+}
