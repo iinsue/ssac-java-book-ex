dbcda4630893958921ed1336cf880c56d7f8dc21
iinsue <hinsu93@naver.com>
2021-12-31 15:09:30 +0900

    chapter 6.7.4 Constructor Overloading

---
+++ chap06/examples/Car2/Car.java
@@ -0,0 +1,31 @@
+package examples.Car2;
+
+// This is Java chapter6 211p
+// chapter 6.7.4 생성자 오버로딩(Overloading)
+
+// 생성자 오버로딩(Overloading)
+public class Car {
+    //Field
+    String company = "현대자동차";
+    String model;
+    String color;
+    int maxSpeed;
+
+    //생성자(Contructor)
+
+    //생성자-1
+    Car() {}
+    //생성자-2
+    Car(String model) {this.model = model;}
+    //생성자-3
+    Car(String model, String color){
+        this.model = model;
+        this.color = color;
+    }
+    //생성자-4
+    Car(String model, String color, int maxSpeed){
+        this.model = model;
+        this.color = color;
+        this.maxSpeed = maxSpeed;
+    }
+}
---
+++ chap06/examples/Car2/CarExample.java
@@ -0,0 +1,34 @@
+package examples.Car2;
+
+// This is Java chapter6 211p
+// chapter 6.7.4 생성자 오버로딩(Overloading)
+
+// 객체 생성 시 생성자 성택
+public class CarExample {
+    public static void main(String[] args) {
+        // 생성자-1 선택
+        Car car1 = new Car();
+        System.out.println("car1.company : " +  car1.company);
+        System.out.println();
+
+        // 생성자-2 선택
+        Car car2 = new Car("자가용");
+        System.out.println("car2.company : " + car2.company);
+        System.out.println("car2.model : " + car2.model);
+        System.out.println();
+
+        // 생성자-3 선택
+        Car car3 = new Car("자가용", "빨강");
+        System.out.println("car3.company : " + car3.company);
+        System.out.println("car3.model : " + car3.model);
+        System.out.println("car3.color : " + car3.color);
+        System.out.println();
+
+        // 생성자-4 선택
+        Car car4 = new Car("택시", "검정", 200);
+        System.out.println("car4.company : " + car4.company);
+        System.out.println("car4.model : " + car4.model);
+        System.out.println("car4.color : " + car4.color);
+        System.out.println("car4.maxSpeed : " + car4.maxSpeed);
+    }
+}
